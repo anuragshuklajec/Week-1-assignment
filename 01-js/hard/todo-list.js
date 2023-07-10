30517ff86ea390c0fc06dd7b19d75319adc8cef2
anuragshuklajec <anuragshuklajec@gmail.com>
2023-07-10 17:39:28 +0530

    completed todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -13,6 +13,50 @@
 
 class Todo {
 
+  constructor(todo){
+    this.todo = todo
+  }
+
+  add(task){
+    this.todo.push(task)
+    return this.todo
+  }
+
+  remove(index){
+    this.todo.splice(index,1)
+    return this.todo
+  }
+
+  update(index,task){
+    this.todo[index] = task
+    return this.todo
+
+  }
+
+  getAll(){
+    return this.todo
+  }
+
+  get(index){
+    return this.todo[index]
+  }
+
+  clear(){
+    this.todo = []
+    return this.todo
+  }
+
 }
+
+var result = new Todo([])
+console.log(result.add(1));
+console.log(result.add(2));
+console.log(result.remove(1));
+console.log(result.update(0,5));
+console.log(result.add(6));
+console.log(result.add(8));
+console.log(result.get(2));
+console.log(result.clear());
+
 
 module.exports = Todo;
